adb1523db52b2a096624c073e63cbb155779f1a7
abiudev <bdmasinde@gmail.com>
2025-01-25 22:34:28 +0300

    et

--- src/components/tik.tsx
+++ src/components/tik.tsx
@@ -1,9 +1,7 @@
 export default function Tik() {
   return (
     <div>
-      <p>
-        tiktok-developers-site-verification=wx9lp6gt4qsedbsE6JwXg1sBymJxPuWW
-      </p>
+      <p>wx9lp6gt4qsedbsE6JwXg1sBymJxPuWW</p>
     </div>
   );
 }
